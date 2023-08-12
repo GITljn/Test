aa8f7087ab6c8d87804b2b3473716a0f8f2f8850
刘佳宁 <8782095+giteeljn@user.noreply.gitee.com>
2023-08-12 09:45:38 +0800

    这是第一个人第二次提交的

--- test1/src/HelloWord.java
+++ test1/src/HelloWord.java
@@ -2,5 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         // 这是第一个人提交的
         System.out.println("hello world");
+        // 这是第一个人第二次提交的
+        System.out.println("一人二次");
     }
 }
