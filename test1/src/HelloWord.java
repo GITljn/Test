83667985605a0135efc3f2bca99ee92796c5d964
刘佳宁 <8782095+giteeljn@user.noreply.gitee.com>
2023-08-12 21:46:27 +0800

    1  1

--- test1/src/HelloWord.java
+++ test1/src/HelloWord.java
@@ -1,26 +1,6 @@
 public class HelloWord {
     public static void main(String[] args) {
-        // 这是第一个人提交的
-        System.out.println("hello world");
-        // 这是第一个人第二次提交的
-        System.out.println("一人二次");
-        // 这是第二个人第三次提交的
-        System.out.println("二人三次");
-
-        // 这是第一个人第三次提交的
-        System.out.println("一人三次");
-
-        // 这是第一个人第四次提交的
-
-        // 这是第一个人第五次提交的
-
-        // 这是第二个人第四次提交的
-        // 这是第二个人第五次提交的
-
-        // 2 6
-
-        // 这是第一个人第五次提交的
-
+        // 1 1
 
     }
 }
